dbfb3b516d0cb7c719ac14ed81f92b84cb7888ff
ryo-is <ryosuke.izumi62@gmail.com>
2024-01-05 18:02:18 +0900

    feat: add UI to add two numbers

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import { add, joinText } from './build/release.js';
 var main = function () {
-    var addResultElm = document.querySelector('#AddResult');
-    if (addResultElm) {
-        addResultElm.innerText = String(add(4, 2));
-    }
+    var inputNum1 = document.querySelector('#InputNum_1');
+    var inputNum2 = document.querySelector('#InputNum_2');
+    var submitButton = document.querySelector('#Submit');
+    var resultElm = document.querySelector('#Result');
+    submitButton.addEventListener('click', function () {
+        if (inputNum1 && inputNum2 && submitButton && resultElm) {
+            var num1 = parseInt(inputNum1.value, 10);
+            var num2 = parseInt(inputNum2.value, 10);
+            // eslint-disable-next-line no-restricted-globals
+            if (isNaN(num1) || isNaN(num2)) {
+                resultElm.innerText = '数字を入力してください';
+            }
+            else {
+                var res = add(num1, num2);
+                resultElm.innerText = String(res);
+            }
+        }
+    });
     var joinTextResultElm = document.querySelector('#JoinTextResult');
     if (joinTextResultElm) {
         joinTextResultElm.innerText = joinText('Hello', 'world!!!', ' ');
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,24 @@
 import { add, joinText } from './build/release.js';
 
 const main = (): void => {
-  const addResultElm = document.querySelector('#AddResult') as HTMLDivElement;
-  if (addResultElm) {
-    addResultElm.innerText = String(add(4, 2));
-  }
+  const inputNum1 = document.querySelector('#InputNum_1') as HTMLInputElement;
+  const inputNum2 = document.querySelector('#InputNum_2') as HTMLInputElement;
+  const submitButton = document.querySelector('#Submit') as HTMLButtonElement;
+  const resultElm = document.querySelector('#Result') as HTMLElement;
+
+  submitButton.addEventListener('click', () => {
+    if (inputNum1 && inputNum2 && submitButton && resultElm) {
+      const num1 = parseInt(inputNum1.value, 10);
+      const num2 = parseInt(inputNum2.value, 10);
+      // eslint-disable-next-line no-restricted-globals
+      if (isNaN(num1) || isNaN(num2)) {
+        resultElm.innerText = '数字を入力してください';
+      } else {
+        const res = add(num1, num2);
+        resultElm.innerText = String(res);
+      }
+    }
+  });
 
   const joinTextResultElm = document.querySelector(
     '#JoinTextResult',
